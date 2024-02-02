11fe983bf27218501a978122b10980b2a739e620
adria <aarivera7@utpl.edu.ec>
2024-02-02 16:18:55 -0500

    Se añadio el modelo del formulario 2 y 3

---
+++ lib/config/model/form2.dart
@@ -0,0 +1,44 @@
+class Form2 {
+  String? nombreUMAController;
+  String? claveRController;
+  String? institucionAsController;
+  String? propietarioRSocController;
+  String? titularRepController;
+  String? telefonoPropController;
+  String? faxPropController;
+  String? correoPropController;
+  String? rfcPropController;
+  String? propRespoTecController;
+  String? titularTecController;
+  String? telefonoTecController;
+  String? faxTecController;
+  String? correoTecController;
+  String? rfcTecController;
+  String? tipo;
+  String? url;
+  double? latitude;
+  double? longitude;
+
+  Form2({
+    this.nombreUMAController,
+    this.claveRController,
+    this.institucionAsController,
+    this.propietarioRSocController,
+    this.titularRepController,
+    this.telefonoPropController,
+    this.faxPropController,
+    this.correoPropController,
+    this.rfcPropController,
+    this.propRespoTecController,
+    this.titularTecController,
+    this.telefonoTecController,
+    this.faxTecController,
+    this.correoTecController,
+    this.rfcTecController,
+    this.tipo,
+    this.url,
+    this.latitude,
+    this.longitude,
+  });
+
+}
---
+++ lib/config/model/form3.dart
@@ -0,0 +1,46 @@
+class Form3 {
+  String? fechaController;
+  String? areaController;
+  String? tipoDisController;
+  String? propietarioController;
+  String? telefonoController;
+  String? nombreParcController;
+  String? fechaMoniController;
+  String? superfMonController;
+  String? numeroArController;
+  String? especieController;
+  String? alturaController;
+  String? diametroController;
+  String? diametroEController;
+  String? diametroNController;
+  String? estadoFitoController;
+  String? mantenimientoController;
+  String? url;
+  double? latitude;
+  double? longitude;
+  String? letter;
+
+  Form3({
+    this.fechaController,
+    this.areaController,
+    this.tipoDisController,
+    this.propietarioController,
+    this.telefonoController,
+    this.nombreParcController,
+    this.fechaMoniController,
+    this.superfMonController,
+    this.numeroArController,
+    this.especieController,
+    this.alturaController,
+    this.diametroController,
+    this.diametroEController,
+    this.diametroNController,
+    this.estadoFitoController,
+    this.mantenimientoController,
+    this.url,
+    this.latitude,
+    this.longitude,
+    this.letter,
+  });
+
+}
